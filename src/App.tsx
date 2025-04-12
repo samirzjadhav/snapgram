30c8b7e4dbf4a7c0cdf77b2964d9237eb34e4b40
samir jadhav <samirzjadhav@gmail.com>
2025-04-12 13:01:17 +0530

    feat: added new files on it

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Routes, Router, Route } from "react-router-dom";
 import "./index.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages";
+import SignupForm from "./_auth/forms/SignupForm";
 
 function App() {
   return (
@@ -8,6 +11,7 @@ function App() {
       <Routes>
         {/* Public routes */}
         <Route path="/sign-in" element={<SigninForm />} />
+        <Route path="/sign-in" element={<SignupForm />} />
 
         {/* Private routes */}
         <Route index element={<Home />} />
