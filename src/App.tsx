699459d05a01ca55ca13ac9cc97bca7d51a91547
samir jadhav <samirzjadhav@gmail.com>
2025-04-13 13:19:19 +0200

    added path and routes on it

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import "./index.css";
 import SigninForm from "./_auth/forms/SigninForm";
 import { Home } from "./_root/pages";
 import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
 
 function App() {
   return (
     <main className="flex h-screen">
       <Routes>
         {/* Public routes */}
-        <Route path="/sign-in" element={<SigninForm />} />
-        <Route path="/sign-in" element={<SignupForm />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-in" element={<SignupForm />} />
+        </Route>
 
         {/* Private routes */}
         <Route index element={<Home />} />
