b573b50306f8704e00f3023bf06b51df81c3e7cb
samir jadhav <samirzjadhav@gmail.com>
2025-04-18 15:13:37 +0530

    feat: added new folder and sign-in and sign-up form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import Home from "./_root/pages/Home";
 
 function App() {
   return (
     <>
-      <h1 className=" text-[100px] text-black  ">hello</h1>
+      <main className="flex h-screen">
+        <Routes>
+          {/* Public routes */}
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+
+          {/* Private Routes */}
+          <Route index element={<Home />} />
+        </Routes>
+      </main>
     </>
   );
 }
